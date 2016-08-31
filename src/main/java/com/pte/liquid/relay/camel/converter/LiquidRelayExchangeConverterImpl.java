d779f8ab000f5d73f4d525ddf81ec172ba424b37
rjptegelaar <tegelaarpaul@gmail.com>
2016-08-31 16:54:23 +0200

    changed logger

--- src/main/java/com/pte/liquid/relay/camel/converter/LiquidRelayExchangeConverterImpl.java
+++ src/main/java/com/pte/liquid/relay/camel/converter/LiquidRelayExchangeConverterImpl.java
@@ -17,11 +17,10 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Logger;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.MessageHistory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.pte.liquid.relay.Constants;
 import com.pte.liquid.relay.Converter;
@@ -30,7 +29,7 @@ import com.pte.liquid.relay.model.Message;
 
 public class LiquidRelayExchangeConverterImpl implements Converter<Exchange>{
 
-	private static final transient Logger LOG = LoggerFactory.getLogger(LiquidRelayExchangeConverterImpl.class);
+	private static final transient Logger LOG = Logger.getLogger("LiquidRelayExchangeConverterImpl");
 
 	private final static String ESB_TYPE_PROPERTY_VALUE = "APACHE_CAMEL";
 	
